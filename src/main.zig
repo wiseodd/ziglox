b1ad2a4c4d57092cfefbc62357de5f6b8931e86f
Agustinus Kristiadi <agustinus@kristia.de>
2024-08-17 09:22:53 -0400

    Handle memory leak

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,10 @@ const dbg = @import("debug.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    defer {
+        const status = gpa.deinit();
+        if (status == .leak) std.debug.print("Memory leak detected!\n", .{});
+    }
 
     var chunk = chk.Chunk.init(allocator);
     try chunk.append(chk.OpCode.OpReturn);
